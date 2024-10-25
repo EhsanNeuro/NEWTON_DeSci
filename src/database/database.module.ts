09b64c60a84cabd33b2519ff1c02d3863f5e5f75
amirAlamian <amiralamian10@gmail.com>
2024-10-25 22:13:29 +0330

    fix: fix injection error on game service

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,10 +1,11 @@
 import { PrismaService } from '@app/database/database.service';
+import { GameRepository } from '@app/database/repositories/game/game.repository';
 import { UserRepository } from '@app/database/repositories/user/user.repository';
 import { Global, Module } from '@nestjs/common';
 
 @Global()
 @Module({
-  providers: [PrismaService, UserRepository],
-  exports: [UserRepository],
+  providers: [PrismaService, UserRepository, GameRepository],
+  exports: [UserRepository, GameRepository],
 })
 export class DatabaseModule {}
